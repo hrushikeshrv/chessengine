ebb4eb5e5700a7b070c6b182cab5482864aa555f
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-06-06 14:50:33 +0530

    Initialized lookup tables

---
+++ src/lookup_tables.py
@@ -0,0 +1,46 @@
+"""
+Lookup tables are bitboards that allow us to mask or clear specific ranks or files of the chessboard
+"""
+clear_rank = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+}
+
+mask_rank = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+}
+
+clear_file = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+}
+
+mask_file = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+}
