313e6de1e9487489c5a0fa8a99e30dbf022e0c52
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-06-07 20:56:34 +0530

    Added helper methods to get piece bitboards

--- src/bitboard.py
+++ src/bitboard.py
@@ -3,7 +3,7 @@ from .lookup_tables import clear_rank, mask_rank, clear_file, mask_file
 
 class Board:
     """
-    A class representing a bitboard
+    A class representing a bitboard representation of the chess board
     """
 
     def __init__(self, side):
@@ -38,17 +38,91 @@ class Board:
             | self.black_queen
             | self.black_king
         )
-        
+
         self.all_pieces = self.all_black | self.all_white
-        self.side = side
+        self.side = side.lower().strip()
 
     def identify_valid_moves(self):
         """
         Generator that identifies all valid moves for all pieces on the board on
-        the computer's side and yields Board objects
+        the computer's side and yields Board objects for each move
         """
-    
-    def move(self, final_board):
+
+    def get_self_piece_bitboard(self, piece: str):
+        """
+        Returns the attribute corresponding to the passed piece, considering the board's
+        own side. i.e. - If the board is white, calling with piece = 'king' will return
+        white king, etc.
+        piece can be one of - "king", "queen", "bishop", "knight", "rook", "pawn"
+        """
+        piece = piece.lower().strip()
+        if piece not in {
+            "king",
+            "queen",
+            "bishop",
+            "knight",
+            "rook",
+            "pawn",
+        }:
+            raise ValueError(
+                f"get_self_piece_bitboard got unknown piece.\nExpected one of {{'king', 'queen', 'bishop', 'knight', "
+                f"'rook', 'pawn'}}, got {piece} instead."
+            )
+        
+        if self.side == 'white':
+            if piece == 'pawn':
+                return self.white_pawns
+            if piece == 'queen':
+                return self.white_queen
+            if piece == 'bishop':
+                return self.white_bishops
+            if piece == 'knight':
+                return self.white_knights
+            if piece == 'rook':
+                return self.white_rooks
+            return self.white_king
+        else:
+            if piece == 'pawn':
+                return self.black_pawns
+            if piece == 'queen':
+                return self.black_queen
+            if piece == 'bishop':
+                return self.black_bishops
+            if piece == 'knight':
+                return self.black_knights
+            if piece == 'rook':
+                return self.black_rooks
+            return self.black_king
+
+    def get_piece_bitboard(self, side: str, piece: str):
+        """
+        Similar to get_self_piece_bitboard, but can be used to get bitboard of any piece
+        for any side. Returns the bitboard of the passed side for the passed pieces.
+        """
+        piece = piece.lower().strip()
+        if piece not in {
+            "king",
+            "queen",
+            "bishop",
+            "knight",
+            "rook",
+            "pawn",
+        }:
+            raise ValueError(
+                f"get_piece_bitboard got unknown piece.\nExpected one of {{'king', 'queen', 'bishop', 'knight', "
+                f"'rook', 'pawn'}}, got {piece} instead."
+            )
+        if side not in {"black", "white"}:
+            raise ValueError(
+                f"get_piece_bitboard got unknown piece.\nExpected one of {{'white', 'black'}}, "
+                f"got {side} instead."
+            )
+        attrname = side + '_' + piece
+        if piece not in {"king", "queen"}:
+            attrname += 's'
+        return getattr(self, attrname)
+
+    def move(self, start: int, to: int, piece_bitboard=None):
         """
         
         """
