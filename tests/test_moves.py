3ad5e7a4a24c2859886f12e2ad583261eb3b3d0d
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-06-19 09:18:40 +0530

    Added get_white_pawn_moves test

--- tests/test_moves.py
+++ tests/test_moves.py
@@ -8,6 +8,9 @@ class TestMoves(unittest.TestCase):
         board = Board('white')
         moves = get_white_pawn_moves(board, 2**8, False)
         self.assertEqual(moves, [2**16, 2**24])
+        
+        board.move(2**9, 2**16)
+        self.assertEqual(get_white_pawn_moves(board, 2**8, False), [])
 
 
 if __name__ == '__main__':
