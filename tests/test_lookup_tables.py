313eddbdabc1cba6bb6861f6be3ede710b53740f
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-07-30 08:40:22 +0530

    Refactored tests

--- tests/test_lookup_tables.py
+++ tests/test_lookup_tables.py
@@ -1,19 +1,8 @@
 import unittest
-from chessengine.utils import lsb_pos
 
 
 class TestLookupTables(unittest.TestCase):
-    def test_lsb_pos(self):
-        test_cases = (
-            (0b1010, 0b10),
-            (0b1110010, 0b10),
-            (0b1000010, 0b10),
-            (0b1001, 0b1),
-            (0b1, 0b1),
-        )
-        for arg, expected in test_cases:
-            with self.subTest(arg=arg):
-                self.assertEqual(lsb_pos(arg), expected)
+    pass
 
 
 if __name__ == "__main__":
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from chessengine.utils import get_bit_positions
+from chessengine.utils import get_bit_positions, lsb_pos
 
 import unittest
 
@@ -6,6 +6,18 @@ import unittest
 class TestUtils(unittest.TestCase):
     def test_get_bit_positions(self):
         self.assertEqual(get_bit_positions(0b110100), [0b100, 0b10000, 0b100000])
+    
+    def test_lsb_pos(self):
+        test_cases = (
+            (0b1010, 0b10),
+            (0b1110010, 0b10),
+            (0b1000010, 0b10),
+            (0b1001, 0b1),
+            (0b1, 0b1),
+        )
+        for arg, expected in test_cases:
+            with self.subTest(arg=arg):
+                self.assertEqual(lsb_pos(arg), expected)
 
 
 if __name__ == '__main__':
