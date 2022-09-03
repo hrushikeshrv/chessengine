eeb9f13be5292ac08ac0c302f51f5c618503f498
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-09-03 18:07:54 +0530

    Updated chessengine.play

--- chessengine/play.py
+++ chessengine/play.py
@@ -1,7 +1,13 @@
 from .bitboard import Board
 
-b = Board("white")
 
 if __name__ == "__main__":
-    print("Board is white, you are black.")
+    player_side = input("Do you want to play white or black (w/b)? - ")
+    while not player_side.lower().strip().startswith(('b', 'w')):
+        print(f'You entered an invalid side - {player_side}. Enter b for black and w for white')
+        player_side = input("Do you want to play white or black (w/b)? - ")
+    if player_side.lower().strip().startswith('w'):
+        b = Board("black")
+    else:
+        b = Board('white')
     b.play()
