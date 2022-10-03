ef00cb25a4df2b8bae96236d5b6f43f334a3cae9
zzril <>
2022-10-03 07:37:38 -0400

    Allow dashes and lowercase piece names

--- chessengine/utils.py
+++ chessengine/utils.py
@@ -3,8 +3,8 @@ Utility functions for common bitboard operations.
 """
 
 
+import re
 from math import log2
-from re import sub
 
 
 piece_characters = {
@@ -82,10 +82,25 @@ def to_san(move: str) -> str:
     Attempts to translate a move in any input format into
     standard algebraic notation (SAN).
     This is intended as a layer between getting the user input
-    and passing it on to the #move_san method in #bitboard.py .
+    and passing it on to the #move_san method in #bitboard.py ,
+    in order to not annoy users who are unfamiliar with SAN.
+    NOTE: If the user input can also contain other, non-move-
+    related instructions (such as ending the game, or asking
+    for a take back), you must parse the move for those
+    instructions before applying this function.
     """
+    def capitalize(match):
+        return match[0].upper()
     # Strip off leading move numbers (like 10... h5 -> h5):
-    move = sub(r'^[1-9][0-9]*\.+ ?', '', move)
-
+    move = re.sub(r'^[1-9][0-9]*\.+ ?', '', move)
+    # Strip off starting square and dashes in straight
+    # (non-capturing) pawn moves (like e2-e4 -> e4):
+    move = re.sub(r'^([a-h])[2-7] ?- ?\1([1-8])', r'\1\2', move)
+    # Allow lowercase piece names (like nf3 -> Nf3):
+    # For pieces that cannot be confused with file names:
+    move = re.sub(r'^[rnqk]', capitalize, move)
+    # For bishops, in cases where it cannot be confused with
+    # the b pawn (like bg5 -> Bg5):
+    move = re.sub(r'^b([d-h][1-8])', r'B\1', move)
     return move
 
