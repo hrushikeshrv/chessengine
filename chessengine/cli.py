305d446f057604f193351d6a82f899ca9b6456d4
Elton Chou <plscd748@gmail.com>
2022-10-03 19:21:28 +0800

    Improve old version compatibility

--- chessengine/cli.py
+++ chessengine/cli.py
@@ -23,18 +23,15 @@ def prompt_player_side():
 
 
 def play(play_with_player: bool):
-    player_side = prompt_player_side()
-    while not player_side.startswith(("b", "w")):
-        print(
-            f"You entered an invalid side - {player_side}. Enter b for black and w for white"
-        )
+    board = Board("white")
+    if play_with_player:
+        board.play_pvp()
+    else:
         player_side = prompt_player_side()
 
-    if player_side.startswith("b"):
-        b = Board("black")
-    else:
-        b = Board("white")
-    b.play()
+        if player_side.startswith("b"):
+            board = Board("black")
+        board.play()
 
 
 def update():
@@ -58,9 +55,13 @@ def main():
     subparsers = parser.add_subparsers(help="Action to perform", dest="action")
     parser_play = subparsers.add_parser("play", help="Start a new game.")
     parser_play.add_argument(
-        "--player", help="Play with other player.", required=False, action="store_true"
+        "-p",
+        "--player",
+        help="Play against another player.",
+        required=False,
+        action="store_true",
     )
-    parser_update = subparsers.add_parser("update", help="Update something")
+    parser_update = subparsers.add_parser("update", help="Update something.")
 
     args = parser.parse_args()
     if args.action == "play":
